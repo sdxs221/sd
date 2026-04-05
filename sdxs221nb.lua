8120d4e02aa5adaa4229129e65902f8991c42e50
sdxs221 <2770184275@qq.com>
2026-04-05 11:11:40 +0800

    创建 sdxs221nb.lua

---
+++ sdxs221nb.lua
@@ -0,0 +1,428 @@
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local rootPart = character:WaitForChild("HumanoidRootPart")
+
+local function simulatePrompt(targetModel)
+    local prompt = targetModel:FindFirstChild("Inside") and targetModel.Inside:FindFirstChild("ProximityPrompt")
+    if prompt then
+        prompt:InputHoldBegin()
+        task.wait(2)
+        prompt:InputHoldEnd()
+    end
+end
+
+local targetModel = workspace:FindFirstChild("10")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("11")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("12")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("13")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("14")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("15")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("16")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("17")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("19")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("2")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("20")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("21")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("22")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("23")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("24")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("25")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("26")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("27")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("28")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("29")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("3")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("30")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("31")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("32")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("33")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("34")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("35")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("36")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("37")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("38")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("39")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("4")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("40")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("41")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("42")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("43")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("44")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("45")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("46")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("47")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("48")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("49")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("5")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("50")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("51")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("52")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("53")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("54")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("6")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("7")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("8")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
+
+targetModel = workspace:FindFirstChild("9")
+if targetModel then
+    rootPart.CFrame = targetModel:GetPivot()
+    task.wait(0.1)
+    simulatePrompt(targetModel)
+end
+task.wait(2)
